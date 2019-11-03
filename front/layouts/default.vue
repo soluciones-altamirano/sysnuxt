9ad0655eb7b8500c8c039fee34d089d1a6f54ee7
Esdras Altamirano <altamiranoesdras@gmail.com>
2019-11-03 12:12:55 -0600

    Mejoras plantilla default.vue

--- front/layouts/default.vue
+++ front/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
-  <v-app dark>
+  <v-app >
 
-    <v-navigation-drawer v-model="drawer" :mini-variant="miniVariant" :clipped="clipped" fixed app>
+    <v-navigation-drawer v-model="drawer" :mini-variant="miniVariant" :clipped="clipped" fixed app >
       <v-list>
         <v-list-item v-for="(item, i) in items" :key="i" :to="item.to" router exact>
           <v-list-item-action>
@@ -15,20 +15,18 @@
       <template v-slot:append>
         <div class="pa-2">
           <v-btn block @click.stop="open = true">
-            <v-icon>mdi-logout</v-icon> SALIR
+            <v-icon>mdi-logout</v-icon>
+            <span  v-show="!miniVariant">SALIR</span>
           </v-btn>
         </div>
       </template>
     </v-navigation-drawer>
 
-    <v-app-bar :clipped-left="clipped" fixed app>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-<!--      <v-btn-->
-<!--        icon-->
-<!--        @click.stop="miniVariant = !miniVariant"-->
-<!--      >-->
-<!--        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>-->
-<!--      </v-btn>-->
+    <v-app-bar :clipped-left="clipped" fixed app dark color="indigo">
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" class="d-flex d-sm-none"/>
+      <v-btn icon @click.stop="miniVariant = !miniVariant" class="d-none d-sm-flex">
+        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
+      </v-btn>
 <!--      <v-btn-->
 <!--        icon-->
 <!--        @click.stop="clipped = !clipped"-->
@@ -44,36 +42,40 @@
       <v-toolbar-title v-text="title" />
       <v-spacer />
 
-<!--      <v-btn icon @click.stop="rightDrawer = !rightDrawer">-->
-<!--        <v-icon>mdi-menu</v-icon>-->
-<!--      </v-btn>-->
+
 
       <v-btn icon @click.stop="open = true">
         <v-icon>mdi-logout</v-icon>
       </v-btn>
 
+      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
+        <v-icon>mdi-settings</v-icon>
+      </v-btn>
+
     </v-app-bar>
 
-    <v-content>
-      <v-container>
+    <v-content light>
+      <v-container  light>
         <nuxt />
 
       </v-container>
     </v-content>
 
 
-<!--    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>-->
-<!--      <v-list>-->
-<!--        <v-list-item @click.native="right = !right">-->
-<!--          <v-list-item-action>-->
-<!--            <v-icon light>-->
-<!--              mdi-repeat-->
-<!--            </v-icon>-->
-<!--          </v-list-item-action>-->
-<!--          <v-list-item-title>Switch drawer (click me)</v-list-item-title>-->
-<!--        </v-list-item>-->
-<!--      </v-list>-->
-<!--    </v-navigation-drawer>-->
+    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
+      <v-list>
+        <v-list-item >
+          <v-list-item-action>
+            <v-icon dark>
+              mdi-palette
+            </v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            Tema
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
 
     <v-footer :fixed="fixed" app>
